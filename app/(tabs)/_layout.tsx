5ea4463bd4c65b58ee7a1b022ca71838b9dc8ce1
Tristan <53378180+JanTristanH@users.noreply.github.com>
2025-12-13 15:59:35 +0000

    remove profile from layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,7 +6,6 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
-import React from 'react';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -40,6 +39,7 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+{/* 
       <Tabs.Screen
         name="profile"
         options={{
@@ -52,7 +52,7 @@ export default function TabLayout() {
             />
           ),
         }}
-      />
+      /> */}
     </Tabs>
   );
 }
