fcef926ce05274ad8624c086332c2ed3b1489057
Jan Tristan Heisecke <jan-tristan.heisecke@centric.eu>
2026-03-13 21:42:40 +0100

    Reset

--- dummyServer.js
+++
@@ -1,6 +0,0 @@
-// server.js
-const http = require('http');
-const server = http.createServer((req, res) => res.end('Hello World'));
-server.listen(8080, '0.0.0.0', () => {
-  console.log('Listening on', server.address());
-});
--- lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export const config = {
-    baseUrl: "https://scaling-umbrella-j757wwx9rj6c5g4v-4004.app.github.dev/odata/v4/api"
-};
